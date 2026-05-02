0d5f276dfcb07221654e0f325d2f59845e916bac
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-05-02 14:02:36 +0300

    Intro to dependency injection container usage

---
+++ src/Storage/Storage.ConsoleApp/InvoiceController.cs
@@ -0,0 +1,69 @@
+﻿using Storage.Domain;
+using Storage.Infra.Contracts;
+using Storage.Infra.Services;
+using Storage.Infra.Validators;
+
+namespace Storage.ConsoleApp;
+
+/*
+    InvoiceController sınıfının bir faturalama sürecini yönettiğini düşünelim.
+    Simülasyon amacıyla gerçek hayatta uygulanan bir çok detayı dışarıda bıraktık.
+    Bir faturanın elektronik ortamdaki dokümanıda bu sürecin bir parçası.
+    
+    Buradaki gereksinimlerden ikisinin de faturanın elektronik dokümanının bir ortama kayıt edilmesi
+    veya oradan çekilmesi olduğunu düşünelim.
+
+    O zaman soru şu: InvoiceController bileşeninin bu işlemler için hangi bileşenlere ihtiyacı var.
+    İhtiyaç duyulan tüm bileşenler Dependency Injection tekniği ile buraya alınabilirler.
+*/
+internal class InvoiceController
+{
+    private readonly IStorageService _storageService;
+    public InvoiceController(IStorageService storageService)
+    {
+        _storageService = storageService;
+    }
+
+    public async Task<bool> SaveInvoice(Invoice invoice)
+    {
+        /*
+            Diyelim ki, AWS Storage kullanılıyor. Aşağıdaki gibi InvoiceController sınıfı içinde,
+            doğrudan AwsS3StorageService sınıfından bir örnek oluşturup onun üzerinden işlemler yapabiliriz.
+            Ancak bu controller başka bir çalışma zamanında(runtime) FileStorageService ile çalışacaksa
+            buraya gelip kodu değiştirmek gerekir, ya da burada if/switch vs kullanıp
+            neye göre hangi storage service'inin kullanılacağına karar vermek gerekir. 
+            Bu da kodun bakımını zorlaştırır.
+
+            Bu kodda çalışır ama sadece AWS3StorageService ile çalışır. Bunun yerine Constructor Injection ile
+            bağımlı olduğumuz bileşenin sözleşmesini(interface) dışarıdan alıp kullanabiliriz.
+            Kim olacağına InvoiceController sınıfını kullanan yer karar verir.
+
+        */
+        //var s3Storage = new AwsS3StorageService("East-1-InvoiceBucket");
+        //s3Storage.AddValidator(new S3KeyValidator());
+        //await _storageService.SaveAsync(invoice.EInvoice);
+
+        // todo@buraksenyurt AddValidator problemini çözmemiz lazım. IStorageService değil, BaseStorage'a ait çünkü.
+        // Buradan şunu da ortaya koyabiliriz. Validator(Audit) dediğimiz işlevler, StorageService bileşenlerine mi ait olmalı
+        // yoksa bu sorumluluk tamamen farklı bir bileşenin sorumluluğunda mı olmalı?
+        // _storageService.AddValidator(new S3KeyValidator());
+        await _storageService.SaveAsync(invoice.EInvoice);
+
+        return true;
+    }
+
+    public async Task<Invoice> GetInvoice(Guid id)
+    {
+        var eInvoiceContent = await _storageService.LoadAsync(id.ToString());
+        // Faturanın bulunması, belgesini çekilip ona eklenmesi
+        throw new NotImplementedException();
+    }
+}
+
+public class Invoice
+{
+    public Guid ID { get; set; }
+    public DateTime AcceptDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public Asset EInvoice { get; set; }
+}
---
+++ src/Storage/Storage.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Dependecy'leri yönetmek için .Net'in kendi iç DIC mekanizmasını kullanabiliriz.
+    Ama bunun için Microsoft.Extensions.DependencyInjection paketini projemize eklememiz gerekir.
+    
+    dotnet add package Microsoft.Extensions.DependencyInjection
+*/
+
+using Microsoft.Extensions.DependencyInjection;
+using Storage.ConsoleApp;
+using Storage.Domain;
+using Storage.Infra.Contracts;
+using Storage.Infra.Services;
+
+// ServiceCollection ile çalışma zamanında hangi arayüzlerin hangi bileşenlere denk geldiğini
+// DI Container sistemine tanımlayabiliyoruz.
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IStorageService, AwsS3StorageService>() // IStorageService talep edildiğinde AwsS3StorageService örneği verilir.
+    .BuildServiceProvider();
+
+// Diyelimli çalışma zamanında IStorageService'in gerçek implementasyonuna ihtiyacımız oldu.
+// Aşağıdaki gibi DIC üzerinden onun somut örneğini alabilir ve kullanabiliriz
+var storageService = serviceProvider.GetService<IStorageService>();
+
+// InvoiceController'ın ihtiyacı olan asıl servisi buradaki gibi Constructor üzerinden gönderebiliriz.
+var invoiceController = new InvoiceController(storageService);
+await invoiceController.SaveInvoice(new Invoice
+{
+    ID = Guid.NewGuid(),
+    EInvoice = new Asset("invoice-123.pdf", [0x25, 0x50, 0x44, 0x46])
+
+});
